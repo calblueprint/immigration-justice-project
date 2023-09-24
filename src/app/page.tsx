5b2fa48115c60850b59dee350f9dce9428f63f11
David Qing <davidqing6432@gmail.com>
2023-09-24 16:33:29 -0700

    cleaned prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-          immigration justice project
-      </div>
+      <div className={styles.description}>immigration justice project</div>
     </main>
-  )
+  );
 }
