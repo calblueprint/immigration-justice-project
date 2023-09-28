2e0f3b0829609391a48e5bd11cb373aaea7d915f
jinkang-0 <jinkang.fang@gmail.com>
2023-09-28 15:16:28 -0700

    fixed prettier issues

--- src/api/supabase/queries/cases.ts
+++ src/api/supabase/queries/cases.ts
@@ -1,22 +1,21 @@
-import supabase from "../createClient";
+import supabase from '../createClient';
 
 export async function getAllCases() {
-    const { data, error } = await supabase.from("cases").select();
+  const { data, error } = await supabase.from('cases').select();
 
-    if (error) {
-        throw new Error(`An error occurred trying to read cases: ${error}`);
-    }
+  if (error) {
+    throw new Error(`An error occurred trying to read cases: ${error}`);
+  }
 
-    return data;
+  return data;
 }
 
 export async function getAllProfiles() {
-    const { data, error } = await supabase.from("profiles").select();
+  const { data, error } = await supabase.from('profiles').select();
 
-    if (error) {
-        throw new Error(`An error occurred trying to read cases: ${error}`);
-    }
+  if (error) {
+    throw new Error(`An error occurred trying to read cases: ${error}`);
+  }
 
-    return data;
+  return data;
 }
-
