6c32da849d893263f17c76594bf3eaac55a3be42
ccatherinetan <catherine95118@gmail.com>
2023-10-01 17:35:22 -0700

    create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { useState } from 'react';
+import supabase from "../../api/supabase/createClient";
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSignUp = async () => {
+        const { data, error } = await supabase.auth.signUp({
+          email,
+          password
+        })
+        console.log(error)
+      }
+
+    const signInWithEmail = async () => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+        email, password,
+        })
+    }
+    
+    const signOut = async () => {
+        const { error } = await supabase.auth.signOut()
+    }  
+
+    return (
+    <>
+      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <input
+        type="password"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button type="button" onClick={handleSignUp}>Sign up</button>
+      <button type="button" onClick={signInWithEmail}>Sign in</button>
+      <button type="button" onClick={signOut}>Sign out</button>
+    </>
+  )
+}
