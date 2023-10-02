96e119b215cc02dec677005d03a2692d6ab1d82a
ccatherinetan <catherine95118@gmail.com>
2023-10-01 18:32:16 -0700

    pass style checks (prettier and ESLint)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,52 @@
-"use client"
+'use client';
 
 import { useState } from 'react';
-import supabase from "../../api/supabase/createClient";
+import supabase from '../../api/supabase/createClient';
 
 export default function Login() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const handleSignUp = async () => {
-        const { data, error } = await supabase.auth.signUp({
-          email,
-          password
-        })
-        console.log(error)
-      }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+    });
+    // console.log(error);
+  };
 
-    const signInWithEmail = async () => {
-        const { data, error } = await supabase.auth.signInWithPassword({
-        email, 
-        password
-        })
-    }
-    /* get rid of signOut 
+  const signInWithEmail = async () => {
+    await supabase.auth.signInWithPassword({
+      // const { data, error } =
+      email,
+      password,
+    });
+  };
+  /* get rid of signOut 
     const signOut = async () => {
         const { error } = await supabase.auth.signOut()
     } 
     <button type="button" onClick={signOut}>Sign out</button>
     */
 
-    return (
+  return (
     <>
-      <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+      <input
+        name="email"
+        onChange={e => setEmail(e.target.value)}
+        value={email}
+      />
       <input
         type="password"
         name="password"
-        onChange={(e) => setPassword(e.target.value)}
+        onChange={e => setPassword(e.target.value)}
         value={password}
       />
-      <button type="button" onClick={handleSignUp}>Sign up</button>
-      <button type="button" onClick={signInWithEmail}>Sign in</button>
+      <button type="button" onClick={handleSignUp}>
+        Sign up
+      </button>
+      <button type="button" onClick={signInWithEmail}>
+        Sign in
+      </button>
     </>
-  )
+  );
 }
