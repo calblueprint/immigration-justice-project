2357c952da69189980582b75279459d93d30f09e
Jinkang Fang <67077248+jinkang-0@users.noreply.github.com>
2023-10-14 20:11:50 -0700

    added new eslint rules for @/ (#12)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,21 @@
 module.exports = {
-  extends: ['@calblueprint/eslint-config-react'],
+  extends: [
+    '@calblueprint/eslint-config-react',
+    'plugin:@typescript-eslint/recommended',
+  ],
   rules: {
     // Add any custom rules here
     // Disable the rule that requires React to be in scope -- we don't need this with React 18
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
   },
+  settings: {
+    'import/resolver': {
+      typescript: {}
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import'
+  ]
 };
