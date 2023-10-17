01efdb997047daf7e6a3f1b1d8fad4d8e6d65611
ccatherinetan <64336644+ccatherinetan@users.noreply.github.com>
2023-10-17 14:00:33 -0700

    [feat] create settings page (auth) (#17)
    * create login page
    
    * finish auth
    
    * pass style checks (prettier and ESLint)
    
    * create basic resetpw and signout pages
    
    * add routing from settings -> reset password
    
    * fix prettier/lint
    
    * fix naming to handleSignIn, handleSignUp, rename password field in reset-password
    
    * change password type
    
    * throw more specific error message
    
    * fix style issues

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import supabase from '../../api/supabase/createClient';
+
+export default function Settings() {
+  const { push } = useRouter();
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      throw new Error(`An error occurred trying to sign out: ${error.message}`);
+    }
+    push('/login');
+  };
+  const resetPassword = () => {
+    push('/reset-password');
+  };
+
+  return (
+    <>
+      <button type="button" onClick={handleSignOut}>
+        Sign out
+      </button>
+      <button type="button" onClick={resetPassword}>
+        Reset Password
+      </button>
+    </>
+  );
+}
