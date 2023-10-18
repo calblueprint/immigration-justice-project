57c71d15f94472c79ac3333d470fe40d2098a2c7
spect <jinkang.fang@gmail.com>
2023-10-18 16:32:42 -0700

    resolved package vulnerabilities and eslint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,10 @@ module.exports = {
     // Disable the rule that requires React to be in scope -- we don't need this with React 18
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
+    "jsx-a11y/label-has-associated-control": [ 2, {
+      "assert": "either",
+    }],
+    "import/prefer-default-export": [ "off" ]
   },
   settings: {
     'import/resolver': {
