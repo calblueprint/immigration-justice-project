adea8f9bf8e887496715cfe7687f232cec5ddf15
ccatherinetan <catherine95118@gmail.com>
2023-10-19 14:39:52 -0700

    fix small things

--- src/components/TextInput/styles.tsx
+++ src/components/TextInput/styles.tsx
@@ -7,14 +7,14 @@ const inter = Inter({ subsets: ['latin'] });
 export const InputText = styled.input<{ $error: boolean }>`
   color: ${COLORS.existingInputColor};
   font-size: 1rem; // 16px
-  font-family: ${inter.style};
+  ${inter.style}
   font-weight: 400;
   line-height: 19.2px;
   word-wrap: break-word;
   padding: 0.625rem 1.25rem; // 10px 20px 10px 20px;
   border-radius: 5px;
   border: 1px solid
-    ${({ $error }) => ($error ? COLORS.errorColor : COLORS.existingInputColor)};
+    ${({ $error }) => ($error ? COLORS.errorColor : COLORS.inputSuggestionColor)};
   justify-content: flex-start;
   align-items: flex-start;
   gap: 10px;
@@ -34,7 +34,7 @@ export const InputText = styled.input<{ $error: boolean }>`
 export const InputTitleText = styled.label`
   color: ${COLORS.existingInputColor};
   font-size: 1.5rem; // 24px;
-  font-family: ${inter.style};
+  ${inter.style}
   font-weight: 500;
   line-height: 28.8px;
   word-wrap: break-word;
