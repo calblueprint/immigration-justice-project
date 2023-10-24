59ededa62c4a2f6093a21651a1da519a4d91b7e3
jinkang-0 <jinkang.fang@gmail.com>
2023-10-24 15:13:49 -0700

    added schema changes
    added schema changes

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,5 +1,26 @@
 import { UUID } from 'crypto';
 
+// note: relief is a string
+
+export type ImmigrationLawExperience =
+  | 'No Experience'
+  | 'One Experience'
+  | 'Multiple Experiences';
+
+export type Role =
+  | 'Attorney'
+  | 'Interpreter'
+  | 'Research Fellow'
+  | 'Translator';
+
+export type ListingType = 'Case' | 'Limited Assistance' | 'Translation Request';
+
+export interface Language {
+  iso_code: string;
+  can_read: boolean;
+  can_write: boolean;
+}
+
 export interface CaseListing {
   id: UUID;
   title: string;
@@ -9,16 +30,39 @@ export interface CaseListing {
   legal_server_id: number;
   hours_per_month: number;
   num_months: number;
-  languages: string[];
   is_remote: boolean;
   in_court: boolean;
   needs_attorney: boolean;
   needs_interpreter: boolean;
   upcoming_date: string;
-  relief_sought: string;
-  active: boolean;
 }
 
+export interface Profile {
+  user_id: UUID;
+  first_name: string;
+  last_name: string;
+  preferred_first_name: string;
+  location: string;
+  hours_per_month: number;
+  immigration_law_experience: ImmigrationLawExperience;
+  bar_number: string;
+  start_date: string;
+  availability_description: string;
+  eoir_registered: boolean;
+}
+
+export interface Interest {
+  listing_id: UUID;
+  user_id: UUID;
+  listing_type: ListingType;
+  form_response: {
+    rolesInterested: Role[];
+    interestReason: string;
+    start_date: string;
+  };
+}
+
+// irrelevant until mvp
 export interface LimitedAssistance {
   id: UUID;
   summary: string;
@@ -34,32 +78,4 @@ export interface TranslationRequest {
   summary: string;
   languages: string[];
   interest_ids: UUID[];
-}
-
-export interface Profile {
-  user_id: UUID;
-  first_name: string;
-  last_name: string;
-  location: string;
-  roles: string[];
-  hours_per_month: number;
-  immigration_law_experience: string;
-  languages: string[];
-  bar_number: string;
-  start_date: string;
-  availability_description: string;
-  eoir_registered: boolean;
-  interest_ids: UUID[];
-}
-
-export interface Interest {
-  id: UUID;
-  listing_id: UUID;
-  user_id: UUID;
-  listing_type: number;
-  form_response: {
-    interestType: string;
-    interestReason: string;
-    start_date: string;
-  };
 }
