3b5cb41ed7e9f0bdf3c8c341e2aae54acd520b35
ccatherinetan <catherine95118@gmail.com>
2023-10-24 15:43:23 -0700

    fix input-text, include the new params for TextInput component

--- src/app/input-text/page.tsx
+++ src/app/input-text/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '../../components/TextInput';
 
 export default function InputTextTest() {
+  const [val, setVal] = useState('');
   return (
     <TextInput
       label="Label"
       placeholder="Input Suggestion"
       erroring={false}
       errorText="Error Text"
+      type="email"
+      name="email"
+      value={val}
+      setValue={setVal}
     />
   );
 }
