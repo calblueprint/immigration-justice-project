ec61e6e48d95ef4b2dd314b425952d46e12cd382
rahihazra <rahi.hazra@gmail.com>
2023-10-26 13:24:37 -0700

    Creates big button for onboarding and auth

---
+++ src/app/button-test/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import React from 'react';
+import BigButton from '@/components/BigButton';
+
+export default function ButtonTest() {
+  return <BigButton>Big Button</BigButton>;
+}
