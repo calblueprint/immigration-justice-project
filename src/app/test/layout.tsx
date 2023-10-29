a5588d6234f9eaf4b06d236714c8ba1c9915963c
jinkang-0 <jinkang.fang@gmail.com>
2023-10-29 15:48:33 -0700

    remove test page

--- src/app/test/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import ProfileProvider from '@/utils/ProfileProvider';
-import { ReactNode } from 'react';
-
-export default function layout({ children }: { children: ReactNode }) {
-  return <ProfileProvider>{children}</ProfileProvider>;
-}
