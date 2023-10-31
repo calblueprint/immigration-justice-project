f883a249904f70e62ef36f5bf86ba0cdaeb250ae
ccatherinetan <catherine95118@gmail.com>
2023-10-31 16:22:51 -0700

    update styles/colors and styles/text with figma designs

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,12 +1,21 @@
 const COLORS = {
-  attorneyColor: '#85b4d2',
-  abaBlue: '#0069A9',
+  // Blues 
+  primaryBlue: '#0069A9', // tearful-blue
+  secondaryBlue: '#035D85', // grieving 
+  tertiaryBlue: '#003453', // drowning 
+  lightPrimaryBlue: '#CCE1EE',
+  lighterPrimaryBlue: '#E8F6FF',
+  // Greys
+  lightGrey: '#F2F2F2',
+  midGey: '#CCC',
+  midDarkGrey: '#818181',
+  darkGrey: '#565656',
   dark: '#292929',
-  interpreterColor: '#8dd89f',
-  inputSuggestionColor: '#B3B3B3', // light grey
-  existingInputColor: '#555555', // dark grey
-  errorColor: '#D13E40', // red
-  activeGreen: '#097A62',
+  placeholderText: '#B3B3B3',
+  // Error Red 
+  errorPrimary: '#D13E40',
+  errorSecondary: '#F3B1B2',
+  errorTertiary: '#FFE9E9',
 };
 
 export default COLORS;
--- src/styles/text.tsx
+++ src/styles/text.tsx
@@ -1,13 +1,43 @@
 import styled from 'styled-components';
+import { Open_Sans as OpenSans } from 'next/font/google';
+import COLORS from './colors';
+
+const openSans = OpenSans({ subsets: ['latin'] });
+
+// 1 rem = 16 px 
 
 export const H1 = styled.h1`
+  ${openSans.style}
   display: block;
-  font-size: 2rem;
+  font-size: 3rem; 
+  font-weight: 700;
+  color: ${COLORS.dark};
   margin: 0;
   margin-right: auto;
 `;
 
 export const H2 = styled.h2`
-  font-size: 1.5rem;
+  font-size: 2.25rem; 
+  font-weight: 600;
+  color: ${COLORS.dark};
   margin: 0;
 `;
+
+export const H3 = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: ${COLORS.dark};
+  margin: 0;
+`;
+
+export const H4 = styled.h4`
+  font-size: 1rem; 
+  font-weight: 600;
+  color: ${COLORS.dark};
+`
+
+export const p = styled.p`
+  font-size: 0.875rem; 
+  font-weight: 400;
+  color: ${COLORS.dark};
+`
