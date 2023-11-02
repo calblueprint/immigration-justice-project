39915c74071ee94907be2dd017d51599ca8917ae
rahihazra <rahi.hazra@gmail.com>
2023-11-02 15:44:31 -0700

    removes test page

--- src/app/button-test/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import React from 'react';
-import BigButton from '@/components/BigButton';
-
-export default function ButtonTest() {
-  return <BigButton>Big Button</BigButton>;
-}
--- src/components/BigButton.tsx
+++ src/components/BigButton.tsx
@@ -11,3 +11,23 @@ const BigButton = styled.button<{ $color?: string }>`
 `;
 
 export default BigButton;
+
+/**
+  'use client';
+
+  import React from 'react';
+  import BigButton from '@/components/BigButton';
+
+  export default function ButtonTest() {
+    return (
+      <BigButton
+        color="yellow"
+        onClick={() => {
+          console.error('hi!');
+        }}
+      >
+        Big Button
+      </BigButton>
+    );
+  }
+ */
