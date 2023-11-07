59986d7a34549eea845a08f0de209517fa437eb0
ccatherinetan <catherine95118@gmail.com>
2023-11-07 14:42:37 -0800

    reset after rebasing

--- src/app/auth/styles.tsx
+++ src/app/auth/styles.tsx
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 import COLORS from '@/styles/colors';
+import { H4 } from '@/styles/text';
+
+export const OuterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
 
 export const FormDiv = styled.div`
   display: flex;
@@ -7,7 +16,8 @@ export const FormDiv = styled.div`
   gap: 2.5rem; // 40 px
   padding: 3.25rem; //52px = 13/4
   border-radius: 0.625rem;
-  border: 2px solid ${COLORS.activeBlue};
+  border: 2px solid ${COLORS.blueMid};
+  width: 580px;
 `;
 
 export const QuestionsDiv = styled.div`
@@ -16,8 +26,13 @@ export const QuestionsDiv = styled.div`
   gap: 1.2rem; // 20 px
 `;
 
-export const SixteenDiv = styled.div`
+export const SpacerDiv = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+`;
+
+export const H4Centered = styled(H4)`
+  color: ${COLORS.greyDark};
+  text-align: center;
 `;
--- src/styles/text.tsx
+++ src/styles/text.tsx
@@ -4,17 +4,20 @@ import COLORS from './colors';
 
 const openSans = OpenSans({ subsets: ['latin'] });
 
-const openSans = OpenSans({ subsets: ['latin'] });
-
 export const H1 = styled.h1<{ $color?: string }>`
   ${openSans.style}
   display: block;
   font-size: 3rem;
-  font-style: normal;
   font-weight: 700;
-  line-height: normal;
+  color: ${props => props.$color || COLORS.greyDarker};
+  margin: 0;
+`;
+
+export const H2 = styled.h2<{ $color?: string }>`
   ${openSans.style}
-  color: ${COLORS.titleBlack};
+  font-size: 2.25rem;
+  font-weight: 600;
+  color: ${props => props.$color || COLORS.greyDarker};
   margin: 0;
 `;
 
@@ -34,23 +37,12 @@ export const H4 = styled.h4<{ $color?: string }>`
   margin: 0;
 `;
 
-export const H4 = styled.h4`
-  color: ${COLORS.existingInputColor};
-  ${openSans.style}
-  font-size: 1rem;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-`;
-
-export const H4Centered = styled(H4)`
-  text-align: center;
-`;
-
-export const P = styled.p`
+export const P = styled.p<{ $color?: string }>`
   ${openSans.style}
   font-size: 0.875rem;
   font-weight: 400;
+  color: ${props => props.$color || COLORS.greyDarker};
+  margin: 0;
 `;
 
 export const AColored = styled.a<{ $color?: string }>`
