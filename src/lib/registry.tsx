5f1ee49300025bf00c9821bfd9c210490c8a285b
jinkang-0 <jinkang.fang@gmail.com>
2023-11-09 11:34:32 -0800

    address typescript compiler complaints

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-no-useless-fragment */
+
+import { ReactNode, useState } from 'react';
 import { useServerInsertedHTML } from 'next/navigation';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 export default function StyledComponentsRegistry({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   // Only create stylesheet once with lazy initial state
   // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
@@ -16,10 +18,10 @@ export default function StyledComponentsRegistry({
   useServerInsertedHTML(() => {
     const styles = styledComponentsStyleSheet.getStyleElement();
     styledComponentsStyleSheet.instance.clearTag();
-    return styles;
+    return <>{styles}</>;
   });
 
-  if (typeof window !== 'undefined') return children;
+  if (typeof window !== 'undefined') return <>{children}</>;
 
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
