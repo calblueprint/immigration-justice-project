9ae310916af9184938df8f654ac820f6f1c166a5
jinkang-0 <jinkang.fang@gmail.com>
2023-11-14 14:08:28 -0800

    refactor onboarding layout

---
+++ src/app/onboarding/layout.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import BigButton from '@/components/BigButton';
+import ProgressBar from '@/components/ProgressBar';
+import { P } from '@/styles/text';
+import { ReactNode } from 'react';
+import { FormDiv, OuterDiv } from '../(auth)/styles';
+
+export default function layout({ children }: { children: ReactNode }) {
+  // track progress with onboarding context
+  return (
+    <OuterDiv>
+      <FormDiv>
+        <ProgressBar
+          steps={
+            new Set(['Basic Info', 'Languages', 'Legal Experience', 'Done'])
+          }
+          progress={2}
+        />
+        {children}
+        <BigButton>
+          <P $color="white">Continue</P>
+        </BigButton>
+      </FormDiv>
+    </OuterDiv>
+  );
+}
