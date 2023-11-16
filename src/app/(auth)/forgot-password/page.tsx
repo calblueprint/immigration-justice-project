8c3b77a17ec19efb688d44d1e2cbd879f919f937
ccatherinetan <catherine95118@gmail.com>
2023-11-16 15:28:07 -0800

    add email resent confirmation text to reset password

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -2,26 +2,32 @@
 
 import { useState } from 'react';
 import TextInput from '@/components/TextInput/index';
-import { H1, H2, H4 } from '@/styles/text';
+import { H1, H2, H4, P } from '@/styles/text';
 import supabase from '@/api/supabase/createClient';
 import COLORS from '@/styles/colors';
-import { SpacerDiv, OuterDiv, FormDiv } from '@/app/(auth)/styles';
+import {
+  OuterDiv,
+  SpacerDiv,
+  HorizontalDiv,
+  FormDiv,
+} from '@/app/(auth)/styles';
 import BigButton from '@/components/BigButton';
 import Button from '@/components/Button';
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState('');
-  const [emailSent, setEmailSent] = useState(false);
+  const [emailSentCount, setEmailSentCount] = useState(0);
   const sendPasswordResetLink = async () => {
     await supabase.auth.resetPasswordForEmail(email, {
       redirectTo: 'http://localhost:3000/reset-password',
     });
-    setEmailSent(true);
+    setEmailSentCount(emailSentCount + 1);
+    // setEmail(email);
   };
 
   return (
     <OuterDiv>
-      {!emailSent && (
+      {!emailSentCount && (
         <FormDiv>
           <H1>Forgot Password</H1>
           <TextInput
@@ -37,7 +43,7 @@ export default function ForgotPassword() {
           </BigButton>
         </FormDiv>
       )}
-      {emailSent && (
+      {emailSentCount && (
         <FormDiv>
           <SpacerDiv>
             <H2>A password reset link has been sent to your email.</H2>
@@ -45,13 +51,18 @@ export default function ForgotPassword() {
               This link will direct you to the next step. If you didn’t receive
               an email, please click Resend Email.{' '}
             </H4>
-            <Button
-              primaryColor={COLORS.blueMid}
-              secondaryColor={COLORS.blueDark}
-              onClick={() => sendPasswordResetLink()}
-            >
-              <H4 $color="white">Resend Email</H4>
-            </Button>
+            <HorizontalDiv>
+              <Button
+                primaryColor={COLORS.blueMid}
+                secondaryColor={COLORS.blueDark}
+                onClick={() => sendPasswordResetLink()}
+              >
+                <H4 $color="white">Resend Email</H4>
+              </Button>
+              {emailSentCount >= 2 && (
+                <P $color={COLORS.greyMid}>Email has been resent!</P>
+              )}
+            </HorizontalDiv>
           </SpacerDiv>
         </FormDiv>
       )}
