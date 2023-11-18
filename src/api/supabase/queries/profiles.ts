8e4bd50d48db564479cbc0e173fa4f0febff430c
Jinkang Fang <67077248+jinkang-0@users.noreply.github.com>
2023-11-18 13:25:46 -0800

    [chore] modify profile context (#37)
    * update profile context to track languages and roles
    
    * update typechecking

--- src/api/supabase/queries/profiles.ts
+++ src/api/supabase/queries/profiles.ts
@@ -1,16 +1,13 @@
-import { Profile } from '@/types/schema';
+import { Profile, ProfileLanguage, ProfileRole } from '@/types/schema';
 import { UUID } from 'crypto';
 import supabase from '../createClient';
 
 export async function fetchProfiles() {
   const { data, error } = await supabase.from('profiles').select('*');
 
-  if (error) {
-    throw new Error(`Error reading profiles: ${error.message}`);
-  } else {
-    // console.log('Profiles Data:', data);
-    return data;
-  }
+  if (error) throw new Error(`Error reading profiles: ${error.message}`);
+
+  return data;
 }
 
 export async function fetchProfileById(userId: UUID) {
@@ -18,7 +15,8 @@ export async function fetchProfileById(userId: UUID) {
     .from('profiles')
     .select()
     .eq('user_id', userId);
-  if (error) throw error;
+  if (error) throw new Error(`Error fetching profile by ID: ${error.message}`);
+
   return data[0];
 }
 
@@ -29,13 +27,90 @@ export async function insertProfile(profileData: Profile) {
   }
 }
 
-export async function updateProfile(id: UUID, updatedInfo: Partial<Profile>) {
-  const { error } = await supabase
+export async function updateProfile(
+  userId: UUID,
+  updatedInfo: Partial<Profile>,
+) {
+  const { data, error } = await supabase
     .from('profiles')
     .update(updatedInfo)
-    .eq('user_id', id);
+    .eq('user_id', userId)
+    .select();
 
   if (error) {
     throw new Error(`Error updating profile data: ${error.message}`);
   }
+
+  return data[0];
+}
+
+// profiles-languages
+export async function fetchLanguagesById(userId: UUID) {
+  const { data, error } = await supabase
+    .from('profiles-languages')
+    .select()
+    .eq('user_id', userId);
+
+  if (error) throw new Error(`Error fetching languages: ${error.message}`);
+
+  return data;
+}
+
+export async function deleteLanguages(
+  userId: UUID,
+  deleteInfo: ProfileLanguage[],
+) {
+  const { error } = await supabase
+    .from('profiles-languages')
+    .delete()
+    .eq('user_id', userId)
+    .in('iso_code', Array.from(new Set(deleteInfo.map(i => i.iso_code))));
+
+  if (error)
+    throw new Error(`Error deleting profiles-languages: ${error.message}`);
+}
+
+export async function upsertLanguages(updatedInfo: ProfileLanguage[]) {
+  const { data, error } = await supabase
+    .from('profiles-languages')
+    .upsert(updatedInfo)
+    .select();
+
+  if (error)
+    throw new Error(`Error updating profiles-languages: ${error.message}`);
+
+  return data;
+}
+
+// profiles-roles
+export async function fetchRolesById(userId: UUID) {
+  const { data, error } = await supabase
+    .from('profiles-roles')
+    .select()
+    .eq('user_id', userId);
+
+  if (error) throw new Error(`Error fetching roles: ${error.message}`);
+
+  return data;
+}
+
+export async function deleteRoles(userId: UUID, deleteInfo: ProfileRole[]) {
+  const { error } = await supabase
+    .from('profiles-roles')
+    .delete()
+    .eq('user_id', userId)
+    .in('role', Array.from(new Set(deleteInfo.map(i => i.role))));
+
+  if (error) throw new Error(`Error deleting profiles-roles: ${error.message}`);
+}
+
+export async function upsertRoles(updatedInfo: ProfileRole[]) {
+  const { data, error } = await supabase
+    .from('profiles-roles')
+    .upsert(updatedInfo)
+    .select();
+
+  if (error) throw new Error(`Error updating profiles-roles: ${error.message}`);
+
+  return data;
 }
