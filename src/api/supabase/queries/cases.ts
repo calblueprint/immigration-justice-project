88228aa2fbf8b1c89f9a9489fab9b7e0296d8bd5
jinkang-0 <jinkang.fang@gmail.com>
2023-11-18 21:16:41 -0800

    update case query to ignore not needing both roles

--- src/api/supabase/queries/cases.ts
+++ src/api/supabase/queries/cases.ts
@@ -2,13 +2,20 @@ import { UUID } from 'crypto';
 import supabase from '../createClient';
 
 export async function getAllCases() {
-  const { data, error } = await supabase.from('cases').select();
+  const { data, error } = await supabase
+    .from('cases')
+    .select()
+    .or('needs_interpreter.eq.true,needs_attorney.eq.true');
   if (error) throw new Error(`Error fetching all cases: ${error.message}`);
   return data;
 }
 
 export async function getNCases(n: number) {
-  const { data, error } = await supabase.from('cases').select().limit(n);
+  const { data, error } = await supabase
+    .from('cases')
+    .select()
+    .or('needs_interpreter.eq.true,needs_attorney.eq.true')
+    .limit(n);
   if (error) throw new Error(`Error fetching ${n} cases: ${error.message}`);
   return data;
 }
