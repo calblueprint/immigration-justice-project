609f5cf44a7be19b76c87ac0b072bba7ea88e94d
Alvaro Ortiz <varortz@berkeley.edu>
2023-11-20 01:13:10 -0800

    chore: prettier

--- src/api/supabase/queries/cases.ts
+++ src/api/supabase/queries/cases.ts
@@ -2,7 +2,7 @@ import { UUID } from 'crypto';
 import { CaseListing } from '@/types/schema';
 import supabase from '../createClient';
 
-export async function getAllCases(): Promise<CaseListing[]>  {
+export async function getAllCases(): Promise<CaseListing[]> {
   const { data, error } = await supabase.rpc('getCasesData');
   if (error) throw new Error(`Error fetching all cases: ${error.message}`);
   return data;
@@ -14,7 +14,7 @@ export async function getNCases(n: number): Promise<CaseListing[]> {
   return data;
 }
 
-export async function getCaseById(id: UUID){
+export async function getCaseById(id: UUID) {
   const { data, error } = await supabase.from('cases').select().eq('id', id);
   if (error) throw new Error(`Error fetching case ${id}: ${error.message}`);
   return data;
