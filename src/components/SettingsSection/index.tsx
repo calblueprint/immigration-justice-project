2e2b2b82c757be3845f15b8c522f45245647a9b6
Jinkang Fang <67077248+jinkang-0@users.noreply.github.com>
2023-11-28 22:29:07 -0800

    [feat] style settings page (#43)
    * finish basics
    
    * add edit button
    
    * add sign out functionality
    
    * update logic to disable account info edit
    
    * reduce top margin for settings

---
+++ src/components/SettingsSection/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { H2, H3, H4, P } from '@/styles/text';
+import {
+  EditButton,
+  KeyValueBlurb,
+  Section,
+  SectionHeader,
+  SectionRow,
+} from './styles';
+
+// types
+interface SectionData {
+  label: string;
+  value: string | string[] | number;
+  format?: (v: string | string[] | number) => string;
+}
+
+interface SettingsSectionProps {
+  title: string;
+  data: Array<SectionData | SectionData[] | string>;
+  editable?: boolean;
+}
+
+// helpers
+const chooseFormatter = (data: SectionData): string => {
+  if (data.format) return data.format(data.value);
+  return data.value instanceof Array
+    ? data.value.join(', ')
+    : data.value.toString();
+};
+
+const dataMapper = (data: SectionData | SectionData[] | string) => {
+  if (typeof data === 'string') return <H3 key={data}>{data}</H3>;
+
+  if (data instanceof Array)
+    return (
+      <SectionRow key={data[0].label}>
+        {data.map(v => (
+          <KeyValueBlurb key={v.label}>
+            <H4>{v.label}</H4>
+            <P>{chooseFormatter(v)}</P>
+          </KeyValueBlurb>
+        ))}
+      </SectionRow>
+    );
+
+  return (
+    <KeyValueBlurb key={data.label}>
+      <H4>{data.label}</H4>
+      <P>{chooseFormatter(data)}</P>
+    </KeyValueBlurb>
+  );
+};
+
+// main component
+export default function SettingsSection({
+  title,
+  data,
+  editable,
+}: SettingsSectionProps) {
+  return (
+    <Section>
+      <SectionHeader>
+        <H2>{title}</H2>
+        {editable && <EditButton />}
+      </SectionHeader>
+      {data.map(dataMapper)}
+    </Section>
+  );
+}
