ad61dbff1bc8ec4100e79fa4391014413314a008
jinkang-0 <jinkang.fang@gmail.com>
2023-11-29 01:20:28 -0800

    revive input dropdown usage

--- src/components/InputDropdown/index.tsx
+++ src/components/InputDropdown/index.tsx
@@ -107,3 +107,128 @@ export default function InputDropdown({
     </DropdownWrapper>
   );
 }
+
+/**
+ * EXAMPLE USAGE
+ * 
+ * Note: data for options should be stored in a constant variable
+ *  OUTSIDE of the function component, or in a useState/useRef.
+ * 
+ * src/app/test/page.tsx:
+
+'use client';
+
+import BigButton from '@/components/BigButton';
+import InputDropdown from '@/components/InputDropdown';
+import { P } from '@/styles/text';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  padding: 4rem 0;
+`;
+
+const Box = styled.div`
+  width: 31.25rem;
+  padding: 4rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 4px 4px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+const elementOptions = new Set([
+  'Argon',
+  'Berkelium',
+  'Carbon',
+  'Darmstadtium',
+  'Einsteinium',
+  'Fluorine',
+  'Gold',
+  'Helium',
+  'Iron',
+  'Krypton',
+  'Lithium',
+  'Magnesium',
+  'Neon',
+  'Oxygen',
+  'Phosphorus',
+  'Radium',
+  'Silicon',
+  'Titanium',
+  'Uranium',
+  'Vanadium',
+  'Xenon',
+  'Ytterbium',
+  'Zinc',
+]);
+
+const disasterOptions = new Set([
+  'Avalanche',
+  'Blizzard',
+  'Cyclone',
+  'Drought',
+  'Earthquake',
+  'Flood',
+  'Hurricane',
+  'Ice storm',
+  'Landslide',
+  'Pandemic',
+  'Ragnorak',
+  'Sinkhole',
+  'Tsunami',
+  'Volcanic Eruption',
+  'Wildfire',
+]);
+
+const emptyOptions = new Set([]);
+
+export default function Page() {
+  const [elements, setElements] = useState<Set<string>>(new Set());
+  const [disaster, setDisaster] = useState<string | null>(null);
+  const [valuesVisible, setValuesVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (elements.size === 0 || !disaster) setValuesVisible(false);
+  }, [elements, disaster]);
+  
+  return (
+    <Container>
+      <Box>
+        <InputDropdown
+          label="Elements"
+          placeholder="Nitrogen"
+          multi
+          onChange={v => setElements(v)}
+          options={elementOptions}
+        />
+        <InputDropdown
+          label="Natural Disaster"
+          placeholder="Hurricane"
+          error="Insufficient funds"
+          onChange={v => setDisaster(v)}
+          options={disasterOptions}
+        />
+        <InputDropdown label="Pandora's Box" disabled options={emptyOptions} />
+        {valuesVisible && (
+          <P>
+            {Array.from(elements).join(', ')} {disaster}
+          </P>
+        )}
+        <BigButton
+          disabled={elements.size === 0 || !disaster}
+          onClick={() => setValuesVisible(!valuesVisible)}
+        >
+          {valuesVisible ? <>Hide Values</> : <>Check Values</>}
+        </BigButton>
+      </Box>
+    </Container>
+  );
+}
+
+ */
