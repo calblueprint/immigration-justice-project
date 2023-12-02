534419f4e89929937064dc30e69b8ad1ed62ce40
ccatherinetan <catherine95118@gmail.com>
2023-12-02 13:05:04 -0800

    implement radio buttons

--- src/app/onboarding/legal-experience/page.tsx
+++ src/app/onboarding/legal-experience/page.tsx
@@ -1,22 +1,25 @@
 'use client';
 
 import { useContext, useState, useEffect } from 'react';
-import Button from '@/components/Button';
-import COLORS from '@/styles/colors';
 import { H1 } from '@/styles/text';
 import { OnboardingContext } from '@/utils/OnboardingProvider';
 import TextInput from '@/components/TextInput';
 import InputDropdown from '@/components/InputDropdown';
+import RadioGroup from '@/components/RadioGroup';
 import { ImmigrationLawExperienceEnum, Profile } from '@/types/schema';
 
-const ExperienceTypes = new Set(['LOW', 'MEDIUM', 'HIGH']);
+const ExperienceTypes = new Set([
+  'Multiple cases of immigration law experience',
+  'Few cases of immigration law experience',
+  'One or no case of immigration law experience',
+]);
 
 export default function Page() {
   const onboarding = useContext(OnboardingContext);
   const [barNum, setBarNum] = useState('');
   const [experience, setExperience] =
     useState<ImmigrationLawExperienceEnum | null>(null);
-  const [registered, setRegistered] = useState<boolean | null>(true);
+  const [registered, setRegistered] = useState('');
 
   const getErrorText = () => {
     if (barNum !== '' && (!/^\d+$/.test(barNum) || barNum.length !== 6)) {
@@ -26,12 +29,12 @@ export default function Page() {
   };
 
   useEffect(() => {
-    if (barNum !== '' && experience !== null && registered !== null) {
+    if (barNum !== '' && experience !== null && registered !== '') {
       // update profile
       const partialProfile: Partial<Profile> = {
         bar_number: barNum,
         immigration_law_experience: experience,
-        eoir_registered: registered,
+        eoir_registered: registered === 'Yes',
       };
       onboarding?.updateProfile(partialProfile);
       // enable continue
@@ -55,18 +58,18 @@ export default function Page() {
       />
       <InputDropdown
         label="What level of immigration law experience do you have?"
-        onChange={v => setExperience(v)}
+        onChange={v => setExperience(v as ImmigrationLawExperienceEnum)}
         options={ExperienceTypes}
-        // id="startDate"
         error="" // "Must select your level of immigration law experience"
       />
-      <Button
-        $primaryColor={COLORS.blueMid}
-        $secondaryColor={COLORS.blueDark}
-        onClick={() => onboarding && onboarding.setCanContinue(true)}
-      >
-        Enable continue
-      </Button>
+      <RadioGroup
+        name="registered"
+        value={registered}
+        setValue={setRegistered}
+        options={['Yes', 'No']}
+        label="Are you registered by the Executive Office of Immigration Review?"
+        error=""
+      />
     </>
   );
 }
