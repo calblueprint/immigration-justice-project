6b210b6acaf2257f667fb97297fa55122bada42d
Pragya Kallanagoudar <pragya.kallanagoudar@gmail.com>
2023-12-02 10:41:22 -0800

    creates roles page

--- src/app/onboarding/roles/page.tsx
+++ src/app/onboarding/roles/page.tsx
@@ -1,24 +1,39 @@
 'use client';
 
-import Button from '@/components/Button';
-import COLORS from '@/styles/colors';
+import InputDropdown from '@/components/InputDropdown';
+import { RoleEnum } from '@/types/schema';
 import { H1 } from '@/styles/text';
 import { OnboardingContext } from '@/utils/OnboardingProvider';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 export default function Page() {
   const onboarding = useContext(OnboardingContext);
+  // const [roles, setRoles] = useState<Set<RoleEnum>>(new Set());
+  const roleOptions = new Set(['Attorney', 'Interpreter']);
+
+  useEffect(() => {
+    onboarding?.setCanContinue(false);
+  }, []);
+
+  const setRoles = (v: Set<RoleEnum>) => {
+    onboarding?.setRoles(v);
+    if (v.size !== 0) {
+      onboarding?.setCanContinue(true);
+    } else {
+      onboarding?.setCanContinue(false);
+    }
+  };
 
   return (
     <>
       <H1>Roles</H1>
-      <Button
-        $primaryColor={COLORS.blueMid}
-        $secondaryColor={COLORS.blueDark}
-        onClick={() => onboarding && onboarding.setCanContinue(true)}
-      >
-        Enable continue
-      </Button>
+
+      <InputDropdown
+        label="What role(s) would you like to hold?"
+        multi
+        onChange={v => setRoles(v as Set<RoleEnum>)}
+        options={roleOptions}
+      ></InputDropdown>
     </>
   );
 }
