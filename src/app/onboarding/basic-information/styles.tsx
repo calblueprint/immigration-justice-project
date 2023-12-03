ca1ae0159f6f7f690360b31eb46b0a61e944193b
rahihazra <rahi.hazra@gmail.com>
2023-12-02 16:44:53 -0800

    making basic info dropdowns

---
+++ src/app/onboarding/basic-information/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import COLORS from '@/styles/colors';
+
+export const LineDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 2rem; // 40 px
+  width: 100%;
+`;
