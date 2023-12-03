3fe179e4a9842d28dd0889a7b5e1dc72f6616e69
pragyakallanagoudar <47801900+pragyakallanagoudar@users.noreply.github.com>
2023-12-02 16:13:27 -0800

    [feat] creates role card (#52)
    * creates roles page
    
    * creates roles page
    
    * adds roles page'
    
    * removes unnecessary cast

--- src/app/onboarding/roles/page.tsx
+++ src/app/onboarding/roles/page.tsx
@@ -1,24 +1,57 @@
 'use client';
 
-import Button from '@/components/Button';
-import COLORS from '@/styles/colors';
+import InputDropdown from '@/components/InputDropdown';
+import { RoleEnum } from '@/types/schema';
 import { H1 } from '@/styles/text';
 import { OnboardingContext } from '@/utils/OnboardingProvider';
 import { useContext } from 'react';
 
+const roleOptions = new Map<RoleEnum, string>([
+  ['ATTORNEY', 'Attorney'],
+  ['INTERPRETER', 'Interpreter'],
+]);
+
 export default function Page() {
   const onboarding = useContext(OnboardingContext);
+
+  const setRoles = (roles: Set<RoleEnum>) => {
+    onboarding?.setRoles(roles);
+    if (roles.size !== 0) {
+      onboarding?.setCanContinue(true);
+    } else {
+      onboarding?.setProgress(0);
+      onboarding?.setCanContinue(false);
+    }
+
+    if (roles.has('ATTORNEY')) {
+      onboarding?.setFlow([
+        { name: 'Roles', url: 'roles' },
+        { name: 'Basic Info', url: 'basic-information' },
+        { name: 'Availability', url: 'availability' },
+        { name: 'Legal Experience', url: 'legal-experience' },
+        { name: 'Done', url: 'done' },
+      ]);
+    } else {
+      onboarding?.setFlow([
+        { name: 'Roles', url: 'roles' },
+        { name: 'Basic Info', url: 'basic-information' },
+        { name: 'Availability', url: 'availability' },
+        { name: 'Done', url: 'done' },
+      ]);
+    }
+  };
 
   return (
     <>
       <H1>Roles</H1>
-      <Button
-        $primaryColor={COLORS.blueMid}
-        $secondaryColor={COLORS.blueDark}
-        onClick={() => onboarding && onboarding.setCanContinue(true)}
-      >
-        Enable continue
-      </Button>
+
+      <InputDropdown
+        label="What role(s) would you like to hold?"
+        multi
+        defaultValue={onboarding?.roles}
+        onChange={v => setRoles(v as Set<RoleEnum>)}
+        options={roleOptions}
+      />
     </>
   );
 }
