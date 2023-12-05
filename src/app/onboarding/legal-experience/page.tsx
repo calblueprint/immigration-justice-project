7e987e018ac097dd7a5d3994e54023a253090412
ccatherinetan <64336644+ccatherinetan@users.noreply.github.com>
2023-12-05 10:53:59 -0800

    [feat] create legal experience card (#51)
    * add background color
    
    * add error handling to login, fr
    
    * add comment
    
    * undo background color
    
    * create page skeleton
    
    * implement radio buttons
    
    * udpate dropdown
    
    * check validBarNum before calling updateProfile
    
    * prepopulates attorney bar number field (others not working rn)
    
    * eslint
    
    * fix things
    
    * remove 2 question marks
    
    * ensures that we can go back to page and edit contents
    
    * stricter bar number validation
    
    * updates bar number validation regex
    
    * changes bar number validation regex
    
    * small changes
    
    * use helper for valid bar num
    
    ---------
    
    Co-authored-by: Pragya Kallanagoudar <pragya.kallanagoudar@gmail.com>
    Co-authored-by: jinkang-0 <jinkang.fang@gmail.com>

--- src/app/onboarding/legal-experience/page.tsx
+++ src/app/onboarding/legal-experience/page.tsx
@@ -1,24 +1,92 @@
 'use client';
 
-import Button from '@/components/Button';
-import COLORS from '@/styles/colors';
+import { useContext, useState, useEffect, useCallback } from 'react';
 import { H1 } from '@/styles/text';
 import { OnboardingContext } from '@/utils/OnboardingProvider';
-import { useContext } from 'react';
+import TextInput from '@/components/TextInput';
+import InputDropdown from '@/components/InputDropdown';
+import RadioGroup from '@/components/RadioGroup';
+import { ImmigrationLawExperienceEnum } from '@/types/schema';
+import { isValidBarNumber } from '@/utils/helpers';
+
+const legalExperienceOptions = new Map<ImmigrationLawExperienceEnum, string>([
+  ['HIGH', '2+ prior immigration cases'],
+  ['MEDIUM', '1 prior immigration case'],
+  ['LOW', '0 prior immigration cases'],
+]);
 
 export default function Page() {
   const onboarding = useContext(OnboardingContext);
+  const [barNum, setBarNum] = useState('');
+  const [registered, setRegistered] = useState('');
+  const [barNumError, setBarNumError] = useState('');
+
+  useEffect(() => {
+    if (!onboarding) return;
+
+    const barNumber = onboarding.profile.bar_number || '';
+    const experience = onboarding.profile.immigration_law_experience;
+    const isRegistered = onboarding.profile.eoir_registered;
+
+    setBarNum(barNumber);
+    if (isRegistered !== undefined)
+      setRegistered(onboarding.profile.eoir_registered ? 'Yes' : 'No');
+
+    if (
+      isValidBarNumber(barNumber) &&
+      experience &&
+      isRegistered !== undefined
+    ) {
+      onboarding.setCanContinue(true);
+    } else {
+      onboarding.setProgress(3);
+      onboarding.setCanContinue(false);
+    }
+  }, [onboarding]);
+
+  const handleBarNumChange = useCallback(
+    (v: string) => {
+      setBarNumError(isValidBarNumber(v) ? '' : 'Invalid bar number');
+      onboarding?.updateProfile({ bar_number: v });
+    },
+    [onboarding],
+  );
 
   return (
     <>
       <H1>Legal Experience</H1>
-      <Button
-        $primaryColor={COLORS.blueMid}
-        $secondaryColor={COLORS.blueDark}
-        onClick={() => onboarding && onboarding.setCanContinue(true)}
-      >
-        Enable continue
-      </Button>
+      <TextInput
+        label="What is your attorney bar number?"
+        placeholder="123456"
+        errorText={barNumError}
+        type="text"
+        id="barNum"
+        value={barNum}
+        setValue={setBarNum}
+        onChange={v => handleBarNumChange(v)}
+      />
+      <InputDropdown
+        label="What level of immigration law experience do you have?"
+        onChange={v =>
+          onboarding?.updateProfile({
+            immigration_law_experience: v as ImmigrationLawExperienceEnum,
+          })
+        }
+        defaultValue={onboarding?.profile.immigration_law_experience}
+        options={legalExperienceOptions}
+        error="" // "Must select your level of immigration law experience"
+      />
+      <RadioGroup
+        name="registered"
+        value={registered}
+        setValue={setRegistered}
+        options={['Yes', 'No']}
+        label="Are you registered by the Executive Office of Immigration Review?"
+        error=""
+        onChange={v =>
+          onboarding?.updateProfile({ eoir_registered: v === 'Yes' })
+        }
+      />
     </>
   );
 }
