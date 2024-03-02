451a2456230f2b5fc7ca264c3ebcf2ab34438145
jinkang-0 <jinkang.fang@gmail.com>
2024-03-02 12:50:16 -0800

    add error message to logging

--- src/app/api/legal-db/route.ts
+++ src/app/api/legal-db/route.ts
@@ -62,73 +62,73 @@ export async function GET() {
   const legalServerData = (await legalServerResponse.json())
     .data as ReportData[];
 
-  const casesData: CasesData[] = legalServerData
-    .filter(
-      lsData =>
-        lsData.matter_builtin_lookup_case_disposition_case_disposition_expn?.toUpperCase() ===
-        'OPEN',
-    )
-    .map(lsData => {
-      const legalProblem =
-        lsData.matter_builtin_lookup_problem_code_legal_problem_code_expn?.toLowerCase();
-      const seekingAsylum = legalProblem?.match(/asylum/);
-      const seekingBond = legalProblem?.match(/bond/);
-      const seekingParole = legalProblem?.match(/parole/);
-      const inRemoval = legalProblem?.match(/removal defense/);
-      const inDetention =
-        lsData.person_builtin_lookup_living_arrangement_living_arrangement_expn
-          ?.toLowerCase()
-          .match(/detention/);
+  const openCases = legalServerData.filter(
+    lsData =>
+      lsData.matter_builtin_lookup_case_disposition_case_disposition_expn?.toUpperCase() ===
+      'OPEN',
+  );
 
-      const caseData: CasesData = {
-        legal_server_id: lsData.identification_number,
-        title: lsData.pb_opportunity_note || 'Migrant seeking representation',
-        summary: lsData.pb_opportunity_summary || '',
-        adjudicating_agency:
-          lsData.custom_custom_matter_custom_lookup_b6a226e574a50b6849998214b82e3570_expn ||
-          '',
-        client_location:
-          lsData.city && lsData.state
-            ? `${lsData.city}, ${lsData.state}`
-            : undefined,
-        country:
-          lsData.matter_builtin_lookup_country_country_of_origin_expn || '',
-        needs_interpreter: lsData.interpreter_needed === 'true',
-        needs_attorney: true,
-      };
+  const casesData: CasesData[] = openCases.map(lsData => {
+    const legalProblem =
+      lsData.matter_builtin_lookup_problem_code_legal_problem_code_expn?.toLowerCase();
+    const seekingAsylum = legalProblem?.match(/asylum/);
+    const seekingBond = legalProblem?.match(/bond/);
+    const seekingParole = legalProblem?.match(/parole/);
+    const inRemoval = legalProblem?.match(/removal defense/);
+    const inDetention =
+      lsData.person_builtin_lookup_living_arrangement_living_arrangement_expn
+        ?.toLowerCase()
+        .match(/detention/);
 
-      // determine time commitment
-      if (inRemoval && seekingAsylum) {
-        caseData.hours_per_month = 45;
-        caseData.num_months = 6;
-      } else if (inDetention && inRemoval) {
-        caseData.hours_per_month = 25;
-        caseData.num_months = 12;
-      } else if (inDetention && seekingAsylum) {
-        caseData.hours_per_month = 40;
-        caseData.num_months = 6;
-      } else if (!inDetention && seekingAsylum) {
-        caseData.hours_per_month = 13;
-        caseData.num_months = 2;
-      } else if (inDetention && seekingParole) {
-        caseData.hours_per_month = 13;
-        caseData.num_months = 1;
-      } else if (inDetention && seekingBond) {
-        caseData.hours_per_month = 17;
-        caseData.num_months = 1;
-      }
+    const caseData: CasesData = {
+      legal_server_id: lsData.identification_number,
+      title: lsData.pb_opportunity_note || 'Migrant seeking representation',
+      summary: lsData.pb_opportunity_summary || '',
+      adjudicating_agency:
+        lsData.custom_custom_matter_custom_lookup_b6a226e574a50b6849998214b82e3570_expn ||
+        '',
+      client_location:
+        lsData.city && lsData.state
+          ? `${lsData.city}, ${lsData.state}`
+          : undefined,
+      country:
+        lsData.matter_builtin_lookup_country_country_of_origin_expn || '',
+      needs_interpreter: lsData.interpreter_needed === 'true',
+      needs_attorney: true,
+    };
 
-      return caseData;
-    });
+    // determine time commitment
+    if (inRemoval && seekingAsylum) {
+      caseData.hours_per_month = 45;
+      caseData.num_months = 6;
+    } else if (inDetention && inRemoval) {
+      caseData.hours_per_month = 25;
+      caseData.num_months = 12;
+    } else if (inDetention && seekingAsylum) {
+      caseData.hours_per_month = 40;
+      caseData.num_months = 6;
+    } else if (!inDetention && seekingAsylum) {
+      caseData.hours_per_month = 13;
+      caseData.num_months = 2;
+    } else if (inDetention && seekingParole) {
+      caseData.hours_per_month = 13;
+      caseData.num_months = 1;
+    } else if (inDetention && seekingBond) {
+      caseData.hours_per_month = 17;
+      caseData.num_months = 1;
+    }
 
-  const languagesData: CaseLanguage[] = legalServerData
+    return caseData;
+  });
+
+  const languagesData: CaseLanguage[] = openCases
     .map(lsData => ({
       listing_id: lsData.identification_number,
       language_name: lsData.person_builtin_lookup_language_language_expn || '',
     }))
     .filter(d => d.language_name);
 
-  const reliefsData: CaseRelief[] = legalServerData
+  const reliefsData: CaseRelief[] = openCases
     .map(lsData => ({
       listing_id: lsData.identification_number,
       relief_code:
@@ -146,7 +146,7 @@ export async function GET() {
   // update cases data
   const { error: casesError } = await supabase.from('cases').upsert(casesData);
   if (casesError)
-    throw new Error('Error updating cases DB. Check logs for details.');
+    throw new Error(`Error updating cases DB: ${casesError.message}`);
 
   // update cases languages
   const { error: langsError } = await supabase
@@ -154,7 +154,7 @@ export async function GET() {
     .upsert(languagesData);
 
   if (langsError)
-    throw new Error('Error updating languages DB. Check logs for details.');
+    throw new Error(`Error updating languages DB: ${langsError.message}`);
 
   // update cases reliefs
   const { error: reliefsError } = await supabase
@@ -162,7 +162,7 @@ export async function GET() {
     .upsert(reliefsData);
 
   if (reliefsError)
-    throw new Error('Error updating reliefs DB. Check logs for details.');
+    throw new Error(`Error updating reliefs DB: ${reliefsError.message}`);
 
   return new Response('Ok');
 }
