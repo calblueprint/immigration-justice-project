6f2c7097b9afadb8dfb86d87eabe107ec070ff15
rahihazra <61635387+rahihazra@users.noreply.github.com>
2024-04-07 17:30:14 -0700

    [feat] fetch and display LCA listings (#67)
    * making basic info dropdowns
    
    * create basic information page
    
    * fetches all LCA listings from supabase and displays in HTML
    
    * iterated feedback on LCA sprint
    
    * chore: removes file introduced during rebasing
    
    * fix: changes implementation to use rpc function
    
    * feat: updates LCA schema
    
    * chore: deleted extra schema file
    
    ---------
    
    Co-authored-by: Alvaro Ortiz <varortz@berkeley.edu>

--- src/api/supabase/queries/limitedCaseAssignments.ts
+++ src/api/supabase/queries/limitedCaseAssignments.ts
@@ -1,7 +1,7 @@
 import { LimitedCaseAssignment } from '@/types/schema';
 import supabase from '../createClient';
 
-export async function getAllLCA(): Promise<LimitedCaseAssignment> {
+export async function getAllLCA(): Promise<LimitedCaseAssignment[]> {
   const { data, error } = await supabase.rpc('get_lca');
 
   if (error) throw new Error(`Error reading LCA: ${error.message}`);
---
+++ src/app/limited-case-assignments/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { LimitedCaseAssignment } from '@/types/schema';
+import { getAllLCA } from '@/api/supabase/queries/limitedCaseAssignments';
+
+/**
+ * Fetches all LCA listings from the database
+ * @returns HTML (unstyled) presenting all LCA objects
+ */
+
+export default function Page() {
+  const [data, setData] = useState<LimitedCaseAssignment[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const LCAData = await getAllLCA();
+        setData(LCAData);
+      } catch (error) {
+        console.error('(useEffect)[LCA]', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Limited Case Assignments</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Listing ID</th>
+            <th>Title</th>
+            <th>Languages</th>
+            <th>Deadline</th>
+            <th>Reserch Topic</th>
+            <th>Summary</th>
+            <th>Deliverable</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(assignment => (
+            <tr key={assignment.id}>
+              <td>{assignment.id}</td>
+              <td>{assignment.title}</td>
+              <td>{assignment.languages}</td>
+              <td>{assignment.deadline}</td>
+              <td>{assignment.research_topic}</td>
+              <td>{assignment.summary}</td>
+              <td>{assignment.deliverable}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
