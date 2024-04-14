194429e317a7a6434f24c8bf0b12ba8ebfe8ba23
ccatherinetan <64336644+ccatherinetan@users.noreply.github.com>
2024-04-13 23:27:00 -0700

    [feat] refactor the CaseDetails component (#72)
    * rename to ListingDetails
    
    * create fields for diff listings
    
    * add icons + add date and summary
    
    * update frontend
    
    * create test page
    
    * styling
    
    * implements logic for custody/client location; case vs case interp
    
    * simplify casefields selection with generic typing
    
    * updates to generic labels
    
    * updates generic date label
    
    * update description icon
    
    * fix styling
    
    * update frontend: move deadline below title
    
    * removes test page
    
    * update research topic + calendar icon
    
    * remove lang support type from highlights
    
    * simplify logic with ternary statements
    
    * fixes parseTimeCommitment
    
    * fix styling
    
    * resolve Subheading nesting
    
    * change rem to px in styles
    
    * modify listingFields to mitigate typing any
    
    * change listingFields into component syntax
    
    * rename interest section
    
    * change not-onboarded text
    
    * check styling

--- src/components/ListingDetails/styles.ts
+++ src/components/ListingDetails/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { H1 } from '@/styles/text';
+import { H1, P } from '@/styles/text';
 import COLORS from '@/styles/colors';
 
 export const CaseDisplay = styled.aside`
@@ -8,29 +8,13 @@ export const CaseDisplay = styled.aside`
 `;
 
 export const CaseTitle = styled(H1)`
-  margin: 0.4rem 4rem;
+  margin: 6.4px 6.4px;
 `;
-
-export const CaseInterestContainer = styled.div`
-  position: sticky;
-  top: 4rem;
-  width: 100%;
-  border-radius: 10px;
-  padding: 3rem;
-  border: 2px solid ${COLORS.blueMid};
-  justify-content: space-between;
-  flex-direction: column;
-  display: block;
-  overflow-y: auto;
-`;
-
 export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   min-height: 50%;
-  gap: 1.35rem;
-  padding: 0.2rem;
 `;
 
 export const InnerInfoContainer = styled.div`
@@ -39,25 +23,42 @@ export const InnerInfoContainer = styled.div`
 `;
 
 export const InnerFieldContainer = styled.div`
-  gap: 1.25rem;
+  gap: 30px;
   justify-content: space-between;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
 `;
 
-export const Line = styled.hr`
-  color: 'black';
-  margin: 1.25rem 0;
-`;
-
 export const FieldContainer = styled.div`
   display: flex;
-  gap: 0.188rem;
+  gap: 3px;
   flex-direction: column;
 `;
 
 export const AuthButtons = styled.div`
   display: flex;
-  gap: 1.5625rem;
-  margin-top: 1.25rem;
+  gap: 25px;
+  margin-top: 20px;
+`;
+
+export const IconTextGroup = styled.div`
+  display: flex;
+  direction: row;
+  gap: 10px;
+  align-items: center;
+`;
+
+export const BorderedSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 30px 55px;
+  border: 1px solid ${COLORS.greyLighter};
+`;
+
+export const Subheading = styled(P)<{ $bold?: true; $color?: string }>`
+  display: inline;
+  font-size: 1rem;
+  font-weight: ${({ $bold }) => ($bold ? 500 : 400)};
+  color: ${({ $color }) => $color || COLORS.greyDarker};
 `;
