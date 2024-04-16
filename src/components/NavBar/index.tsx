6643931ba4dbd298d30cbf76b7078966736e529c
kevin3656 <kevinsolorio@berkeley.edu>
2024-04-15 18:32:59 -0700

    feat: homepage update for active links

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@
 
 import { useContext, useMemo } from 'react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import ProfileButton from '@/components/ProfileButton';
 import COLORS from '@/styles/colors';
 import { ProfileContext } from '@/utils/ProfileProvider';
@@ -9,6 +10,7 @@ import Icon from '../../../assets/icons/Icon';
 import { LinkButton } from '../Buttons';
 import {
   AuthButtons,
+  LinkContainer,
   NavBarContainer,
   NavBarSectionDiv,
   NoUnderlineLink,
@@ -44,22 +46,60 @@ export default function NavBar() {
     );
   }, [profile]);
 
+  function IsActive(path: string): boolean {
+    const currentPath = usePathname();
+    return currentPath.includes(path);
+  }
+
+  type NavLinks = {
+    name: string;
+    path: string;
+    active: boolean;
+  };
+  const navllink: NavLinks[] = [
+    { name: 'Cases', path: '/cases', active: IsActive('/cases') },
+    {
+      name: 'Limited Case Assignments',
+      path: '/cases',
+      active: IsActive('/cses'),
+    },
+    { name: 'Language Support', path: '/cases', active: IsActive('/cses') },
+  ];
+
+  const renderLink = (link: NavLinks) => (
+    <LinkContainer active={link.active}>
+      {/* Wrapper div for vertical centering */}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          flex: 1,
+        }}
+      >
+        <NoUnderlineLink href={link.path} $color="white">
+          {link.name}
+        </NoUnderlineLink>
+      </div>
+      <hr
+        style={{
+          display: 'block',
+          width: '100%',
+          height: '4px',
+          backgroundColor: 'white',
+          border: 'none',
+          margin: 0, // Removing default margin from <hr>
+        }}
+      />
+    </LinkContainer>
+  );
   return (
     <NavBarContainer>
       <NavBarSectionDiv>
         <Link href="/">
           <Icon type="logo" />
         </Link>
-        <NoUnderlineLink href="/cases" $color="white">
-          Cases
-        </NoUnderlineLink>
-
-        <NoUnderlineLink href="/cases" $color="white">
-          Limited Case Assignments
-        </NoUnderlineLink>
-        <NoUnderlineLink href="/cases" $color="white">
-          Language Support
-        </NoUnderlineLink>
+        {navllink.map(NavLinks => renderLink(NavLinks))}
       </NavBarSectionDiv>
       <NavBarSectionDiv>
         <AuthButtons>{AuthButtonView}</AuthButtons>
