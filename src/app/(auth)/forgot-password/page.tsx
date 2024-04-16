bf370a071ca5ece109a7767f45b02327508f2207
Pragya Kallanagoudar <pragya.kallanagoudar@gmail.com>
2024-04-16 12:06:47 -0700

    changes auth titles to be dark blue, changes input text border width

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import isEmail from 'validator/lib/isEmail';
 import supabase from '@/api/supabase/createClient';
 import { HorizontalDiv } from '@/app/(auth)/styles';
-import { BigBlueButton, Button } from '@/components/Buttons';
+import { BigBlueButton } from '@/components/Buttons';
 import TextInput from '@/components/TextInput/index';
 import COLORS from '@/styles/colors';
 import { Flex, SmallCardForm } from '@/styles/containers';
@@ -32,39 +32,41 @@ export default function ForgotPassword() {
     setEmailSentCount(emailSentCount + 1);
   };
 
-  return emailSentCount === 0 ? (
+  return (
     <SmallCardForm onSubmit={handleForgotPassword}>
-      <H1>Forgot Password</H1>
-      <TextInput
-        label="Email"
-        placeholder="email@example.com"
-        type="email"
-        id="email"
-        errorText={emailError}
-        value={email}
-        setValue={setEmail}
-      />
-      <BigBlueButton type="submit">
-        <H4 $color="white">Send link to email</H4>
-      </BigBlueButton>
-    </SmallCardForm>
-  ) : (
-    <SmallCardForm onSubmit={handleForgotPassword}>
-      <Flex $direction="column" $gap="20px">
-        <H2>A password reset link has been sent to your email.</H2>
-        <H4 $color={COLORS.greyDark}>
-          This link will direct you to the next step. If you didn’t receive an
-          email, please click Resend Email.
-        </H4>
-        <HorizontalDiv>
+      {emailSentCount === 0 ? (
+        <>
+          <H1>Forgot Password?</H1>
+          <TextInput
+            label="Email"
+            placeholder="email@example.com"
+            type="email"
+            id="email"
+            errorText={emailError}
+            value={email}
+            setValue={setEmail}
+          />
           <BigBlueButton type="submit">
-            <H4 $color="white">Resend Email</H4>
+            <H4 $color="white">Send link to email</H4>
           </BigBlueButton>
-          {emailSentCount > 1 && (
-            <P $color={COLORS.greyMid}>Email has been resent!</P>
-          )}
-        </HorizontalDiv>
-      </Flex>
+        </>
+      ) : (
+        <Flex $direction="column" $gap="20px">
+          <H2>A password reset link has been sent to your email.</H2>
+          <H4 $color={COLORS.greyDark}>
+            This link will direct you to the next step. If you didn’t receive an
+            email, please click Resend Email.
+          </H4>
+          <HorizontalDiv>
+            <BigBlueButton type="submit">
+              <H4 $color="white">Resend Email</H4>
+            </BigBlueButton>
+            {emailSentCount > 1 && (
+              <P $color={COLORS.greyMid}>Email has been resent!</P>
+            )}
+          </HorizontalDiv>
+        </Flex>
+      )}
     </SmallCardForm>
   );
 }
