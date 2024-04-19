9f1ea60a07f0d34dcaf9d02e4aa7e62677dbf1c1
Pragya Kallanagoudar <pragya.kallanagoudar@gmail.com>
2024-04-18 21:15:12 -0700

    updates the confirm reset password page

--- src/app/(auth)/confirm-reset-password/page.tsx
+++ src/app/(auth)/confirm-reset-password/page.tsx
@@ -2,13 +2,14 @@
 
 import { BigBlueLinkButton } from '@/components/Buttons';
 import { Flex, SmallCard } from '@/styles/containers';
-import { H2, H4 } from '@/styles/text';
+import { H4 } from '@/styles/text';
+import { AuthSubHeading } from '../styles';
 
 export default function ConfirmResetPassword() {
   return (
     <SmallCard>
       <Flex $direction="column" $gap="20px">
-        <H2>Your password has been reset.</H2>
+        <AuthSubHeading>Your password has been reset!</AuthSubHeading>
         <BigBlueLinkButton href="/login">
           <H4 $color="white">Go to Log In</H4>
         </BigBlueLinkButton>
