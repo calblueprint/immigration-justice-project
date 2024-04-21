b8e5baeb4818a7aa70466eb1fc235164967f389b
kevin3656 <kevinsolorio@berkeley.edu>
2024-04-21 15:43:24 -0700

    refactor: renamed to styles.ts

--- src/app/styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const PageContainer = styled.div`
-  min-height: 100svh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding-top: 1rem;
-`;
