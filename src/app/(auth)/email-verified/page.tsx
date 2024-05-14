69e77a39daefe26fd97efddf388533a0e8bf35b5
Pragya Kallanagoudar <pragya.kallanagoudar@gmail.com>
2024-05-13 20:04:46 -0700

    uses CONFIG.onboardingHome instead of /onboarding

--- src/app/(auth)/email-verified/page.tsx
+++ src/app/(auth)/email-verified/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { BigBlueLinkButton } from '@/components/Buttons';
+import CONFIG from '@/lib/configs';
 import { Flex, SmallCard } from '@/styles/containers';
 import { H2 } from '@/styles/text';
 
@@ -9,7 +10,7 @@ export default function EmailVerified() {
     <SmallCard>
       <Flex $direction="column" $gap="20px">
         <H2>Your email has been verified!</H2>
-        <BigBlueLinkButton type="button" href="/onboarding/roles">
+        <BigBlueLinkButton type="button" href={CONFIG.onboardingHome}>
           Go to Onboarding
         </BigBlueLinkButton>
       </Flex>
