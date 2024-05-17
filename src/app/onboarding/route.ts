0a16398469ed32b73a4954e53456a80a0024bfe1
jinkang-0 <jinkang.fang@gmail.com>
2024-05-16 18:48:08 -0700

    declutter workspace

--- src/app/onboarding/route.ts
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-import CONFIG from '@/lib/configs';
-
-export async function GET() {
-  redirect(CONFIG.onboardingHome);
-}
--- src/components/EditButton/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import COLORS from '@/styles/colors';
-import { P } from '@/styles/text';
-
-export const EditText = styled(P)`
-  font-weight: 600;
-  color: ${COLORS.blueMid};
-  text-decoration: underline;
-`;
